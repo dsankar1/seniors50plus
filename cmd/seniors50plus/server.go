4df8f35f6a9b2c528a9e11ee1a57acd6eb00b4c1
dsankar1 <daryan.sankar1@gmail.com>
2018-03-19 03:10:37 -0400

    Commented out TLS stuff

--- cmd/seniors50plus/server.go
+++ cmd/seniors50plus/server.go
@@ -4,15 +4,15 @@ import (
 	"net/http"
 	"seniors50plus/internal/auth"
 	"seniors50plus/internal/middleware"
+	"time"
 
 	"github.com/labstack/echo"
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	e := echo.New()
-	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("daryans-page.com")
-	e.AutoTLSManager.Cache = autocert.DirCache("./var/www/.cache")
+	//e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("daryans-page.com")
+	//e.AutoTLSManager.Cache = autocert.DirCache("./var/www/.cache")
 	middleware.ApplyMiddleware(e)
 
 	e.GET("/api", func(c echo.Context) error {
@@ -21,11 +21,11 @@ func main() {
 	e.POST("/api/authenticate", auth.AuthenticationHandler)
 	e.POST("/api/register", auth.RegistrationHandler)
 
-	/*s := &http.Server{
+	s := &http.Server{
 		Addr:         ":1323",
 		ReadTimeout:  20 * time.Second,
 		WriteTimeout: 20 * time.Second,
-	}*/
-	e.Logger.Fatal(e.StartAutoTLS(":443"))
-	//e.Logger.Fatal(e.StartServer(s))
+	}
+	//e.Logger.Fatal(e.StartAutoTLS(":443"))
+	e.Logger.Fatal(e.StartServer(s))
 }
