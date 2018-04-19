b4812b794521d48dbb4edadc8fcf914cf1014c54
dsankar1 <daryan.sankar1@gmail.com>
2018-04-18 21:16:02 -0400

    merged dev branch

--- cmd/seniors50plus/server.go
+++ cmd/seniors50plus/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net/http"
 	"seniors50plus/internal/routing"
+	"time"
 
 	"github.com/labstack/echo"
 	"golang.org/x/crypto/acme/autocert"
@@ -15,11 +17,11 @@ func main() {
 	routing.RegisterMiddleware(e)
 	routing.RegisterHandlers(e)
 
-	/*s := &http.Server{
+	s := &http.Server{
 		Addr:         ":1323",
 		ReadTimeout:  20 * time.Second,
 		WriteTimeout: 20 * time.Second,
 	}
-	e.Logger.Fatal(e.StartServer(s))*/
-	e.Logger.Fatal(e.StartAutoTLS(":443"))
+	e.Logger.Fatal(e.StartServer(s))
+	//e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
